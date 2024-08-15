330f0b83ab9dbe4a09690e998c959a9b118e6485
Clifford Bernard <clifford@verticalfront.com>
2024-08-15 10:45:08 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import type { FC } from "react";
+
+export const App: FC = () => {
+  return (
+    <>
+      <div>
+        <h1>Test App</h1>
+        <Button>Test Button</Button>
+      </div>
+    </>
+  );
+};
